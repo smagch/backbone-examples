57b82038f2d59406480ddbde33faebfe876ab2aa
smagch <smagch@gmail.com>
2012-11-28 21:13:31 +0900

    override `.sync()` method to cache response

--- filter/js/collections/item.js
+++ filter/js/collections/item.js
@@ -5,10 +5,11 @@
    * Module Dependencies
    */
 
-  var $ = global.$
-    , _ = global._
-    , Backbone = global.Backbone
-    , ItemModel = global.ItemModel;
+  var $ = global.$,
+    _ = global._,
+    LRUCache = global.LRUCache,
+    Backbone = global.Backbone,
+    ItemModel = global.ItemModel;
 
   var types = {
     comment: 1,
@@ -21,6 +22,18 @@
    * parameters
    *   - type: "comment" or "submission"
    *   - sort: "create_ts" or "points" or "num_comments"
+   *
+   * Cache up to recently used 10 requests based of query parameter hash
+   * You may want to use LocalStorage for caching
+   * http://engineering.linkedin.com/mobile/linkedin-ipad-using-local-storage-snappy-mobile-apps
+   *
+   * In this case, it use Isaac Schlueter's lru-cache
+   * https://github.com/isaacs/node-lru-cache
+   *
+   * For real HN app, you'll want to set `maxAge` option in LURCache
+   * since HN Search API reject stale signedId
+   *
+   * Note: It won't work in old browsers since LRUCache uses `Object.defineProperty`
    */
 
   global.ItemCollection = Backbone.Collection.extend({
@@ -28,6 +41,7 @@
     initialize: function () {
       this._selected = null;
       this._params = {};
+      this.cache = new LRUCache(10);
     },
     hasChange: function (params) {
       for (var key in params) {
@@ -48,15 +62,16 @@
       this.trigger('filter', _.clone(this._params), options);
       return this.fetch(options);
     },
-    url: function () {
-      var params = {
+    getFilterHash: function () {
+      return $.param({
         'filter[fields][type]': this._params.type,
         limit: 20,
         sortby: this._params.sort + ' desc'
-      };
-
+      });
+    },
+    url: function () {
       return 'http://api.thriftdb.com/api.hnsearch.com/items/_search?'
-        + $.param(params) + '&callback=?';
+        + this.getFilterHash() + '&callback=?';
     },
     parse: function (res) {
       return res.results;
@@ -89,6 +104,33 @@
       this.trigger('deselect', this._selected);
       this._selected = null;
       return this;
+    },
+    sync: function (method, model, options) {
+      if (method !== 'read') {
+        return Backbone.sync.call(this, method, model, options);
+      }
+      var self = this;
+      var key = this.getFilterHash();
+      var value = this.cache.get(key);
+      var xhr;
+      // set cache
+      if (!value) {
+        xhr = Backbone.sync.call(this, method, model, options);
+        xhr.done(function (res, msg, xhr) {
+          self.cache.set(key, res);
+        });
+        return xhr;
+      }
+      // perform psudo request
+      xhr = $.Deferred();
+      _.defer(function () {
+        // this is always true if it's from `.fetch()` call
+        if (options && typeof options.success === 'function') {
+          options.success(value, 'success', xhr);
+        }
+        xhr.resolve(value, 'success', xhr);
+      });
+      return xhr;
     }
   });
 
